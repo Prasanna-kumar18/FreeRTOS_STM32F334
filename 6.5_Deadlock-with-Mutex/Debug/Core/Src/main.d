48acd37fc7f2545ce0d0bd7dc3c1e09aae534fb7
Prasanna-kumar18 <s.v.prasannakumar1809@gmail.com>
2023-08-30 12:12:35 +0530

    Dead Lock

---
+++ 6.5_Deadlock-with-Mutex/Debug/Core/Src/main.d
@@ -0,0 +1,80 @@
+Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h \
+ ../Core/Inc/stm32f3xx_hal_conf.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f334x8.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_exti.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim_ex.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/projdefs.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/portable.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/task.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/list.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/semphr.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/queue.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/task.h
+../Core/Inc/main.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h:
+../Core/Inc/stm32f3xx_hal_conf.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h:
+../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f334x8.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_exti.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim_ex.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/FreeRTOS.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/projdefs.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/portable.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/task.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/list.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/semphr.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/queue.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.5_Deadlock-with-Mutex/FreeRTOS/include/task.h:
