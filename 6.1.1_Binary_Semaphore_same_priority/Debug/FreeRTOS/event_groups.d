929bcfd7e08e5a89fb9025456d1b65f8f980330d
Prasanna-kumar18 <s.v.prasannakumar1809@gmail.com>
2023-08-30 11:00:01 +0530

    Binary Semaphore

---
+++ 6.1.1_Binary_Semaphore_same_priority/Debug/FreeRTOS/event_groups.d
@@ -0,0 +1,27 @@
+FreeRTOS/event_groups.o: ../FreeRTOS/event_groups.c \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/projdefs.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/portable.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/task.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/list.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/timers.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/task.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/event_groups.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/timers.h
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/FreeRTOS.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/projdefs.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/portable.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/task.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/list.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/timers.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/task.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/event_groups.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/timers.h:
---
+++ 6.1.1_Binary_Semaphore_same_priority/Debug/FreeRTOS/tasks.d
@@ -0,0 +1,25 @@
+FreeRTOS/tasks.o: ../FreeRTOS/tasks.c \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/projdefs.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/portable.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/task.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/list.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/timers.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/task.h \
+ C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/stack_macros.h
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/FreeRTOS.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/projdefs.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/portable.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/task.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/list.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/timers.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/task.h:
+C:/Users/Prasanna\ Kumar/Downloads/IPC\ Codes/6.1.1_Binary_Semaphore_same_priority/FreeRTOS/include/stack_macros.h:
